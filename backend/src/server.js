dc86c31054b15a0244b0f8b4cda573fd7a3b4360
Alexandre <alexmlf@hotmail.com>
2019-11-05 23:54:31 -0300

    Usar rotas

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,6 +5,8 @@ const app = express();
 
 // Plugin para reconhecer req.body de JSON
 app.use(express.json());
+// User as rotas
+app.use(routes)
 
 // Inicia servidor na porta 3333
 app.listen(3333);
