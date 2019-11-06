93ff9e134928317d6f330422f65ab3647d17c827
Alexandre <alexmlf@hotmail.com>
2019-11-05 23:31:38 -0300

    req.body

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,9 +5,13 @@ const app = express();
 // Rota com parâmetros de request e response
 // req.query - Acessar query params (filtros)
 // req.params - Acessar route params (edição, delete)
+// req.body - Acessar corpo da requisição (criação, edição)
 
-app.put('/users/:id', (request, response) => {
-    return response.json({ id: request.params.id})
-})
+// Plugin para reconhecer req.body de JSON
+app.use(express.json());
+
+app.post('/users/', (request, response) => {
+    return response.json(request.body)
+});
 // Porta
 app.listen(3333);
