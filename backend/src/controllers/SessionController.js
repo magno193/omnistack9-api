0e42c37efc091cd8f43465318537e5770c6fb8fd
Alexandre Ferreira <alexmlf93@gmail.com>
2019-11-06 23:00:56 -0300

    Terminado criação de um usuário

--- backend/src/controllers/SessionController.js
+++ backend/src/controllers/SessionController.js
@@ -4,7 +4,13 @@ module.exports = {
     async store(req, res){
         // req.body para pegar a estrutura JSON da requisição
         const email = req.body.email;
-        const user = await User.create(email); // Cria um user
+
+        // Caso não exista um email identico no banco de dados
+        // Cadastra um novo usuario com email
+        let user = await User.findOne({email: email})
+        if (!user){
+            user = await User.create({email: email})
+        }
 
         return res.json(user); // Retorna o user
     }
