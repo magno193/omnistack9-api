f361e8abc94efac6163067c8ad10ff4159e5c7ee
magno193 <34192221+magno193@users.noreply.github.com>
2019-11-08 01:47:39 -0200

    Criação de uma spot pelo o SpotController

--- backend/src/controllers/SpotController.js
+++ backend/src/controllers/SpotController.js
@@ -1,8 +1,19 @@
+const Spot = require('../models/Spot');
+
 module.exports = {
     async store(req, res){
-        console.log(req.body);
-        console.log(req.file);
-        
-        return res.json({ ok: req.body });
+        const filename = req.file.filename;
+        const {company, techs, price} = req.body;
+        const user_id = req.headers.user_id; // Contexto da aplicação
+
+        const spot = await Spot.create({
+            user: user_id,
+            image: filename,
+            company: company,
+            techs: techs.split(',').map(tech => tech.trim()),
+            price: price
+        })
+
+        return res.json(spot);
     }
 }
