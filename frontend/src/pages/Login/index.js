158f569058ab7c5266a134ba7cc58528040c5129
Alexandre Ferreira <alexmlf93@gmail.com>
2019-11-12 23:24:36 -0300

    Correções 2

--- frontend/src/pages/Login/index.js
+++ frontend/src/pages/Login/index.js
@@ -1,41 +1,43 @@
 import React, {useState} from "react";
 import api from '../../services/api';
 
-export default function Login() {
+export default function Login({ history }) {
     // Estados
     // Estado do email e atualizar o email
     const [email, setEmail] = useState('');
   
     async function handleSubmit(event) {
-    // Previna o seu funcionamento padrão
-    event.preventDefault()
+        // Previna o seu funcionamento padrão
+        event.preventDefault()
     
-    // Chamando a api
-    const response = await api.post('/sessions', {
-        email: email
-    });
-    // Pega o id do email
-    const _id = response.data._id
-    // Armazenar o id no BD do navegador
-    localStorage.setItem('user', _id);
+        // Chamando a api
+        const response = await api.post('/sessions', {
+            email: email
+        });
+        // Pega o id do email
+        const _id = response.data._id
+        // Armazenar o id no BD do navegador
+        localStorage.setItem('user', _id);
+
+        history.push('/dashboard')
     }
     
     return (
-    <>
-    <p>
-        Ofereça <strong>spots</strong> para programadores e encontre<strong> talentos</strong> para a sua empresa.
-    </p>
+        <>
+        <p>
+            Ofereça <strong>spots</strong> para programadores e encontre<strong> talentos</strong> para a sua empresa.
+        </p>
 
-    <form onSubmit={handleSubmit}>
-        <label htmlFor="email">E-MAIL *</label>
-        <input
-            required
-            type="email"
-            id="email"
-            placeholder="Seu melhor e-mail!"
-            onChange={event => setEmail(event.target.value)}/>
-        <button className="btn" type="submit">Entrar</button>
-        </form>
-    </>
+        <form onSubmit={handleSubmit}>
+            <label htmlFor="email">E-MAIL *</label>
+            <input
+                required
+                type="email"
+                id="email"
+                placeholder="Seu melhor e-mail!"
+                onChange={event => setEmail(event.target.value)}/>
+            <button className="btn" type="submit">Entrar</button>
+            </form>
+        </>
     );
 }
